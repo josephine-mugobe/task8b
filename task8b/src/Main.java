cb69b162deaf6029d1adad7cde4af1d607252106
josephine-mugobe <josephinemunguacizamugobe@gmail.com>
2025-03-19 19:46:01 +0300

    commit#2
    in the NV on line 8 i've created an instance of the second subclass named nonCitrus.

--- task8b/src/Main.java
+++ task8b/src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         client1.displayFruitDetails();
         System.out.println("the price of those orange is " + client1.FruitCostPrice(500, 6));
         System.out.println();
+
+        nonCitrus client2 = new nonCitrus("Cylindrical and elongated", "B6","yellow");
+        client2.displayFruitDetails();
+        System.out.println("the price of those banana is " + client2.FruitCostPrice(1000, 12));
+        System.out.println();
+
     }
 }
