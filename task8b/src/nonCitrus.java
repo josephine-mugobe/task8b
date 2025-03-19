d4e437fef5db11c868241a341705040bdf8366ef
josephine-mugobe <josephinemunguacizamugobe@gmail.com>
2025-03-19 19:40:03 +0300

    commit#1
    In the old version on line 1 i've remove the r on nonCirtus and become citrus
    in the NV on line 5 i've use the constructor that initialize the the common value and i've set the name of it using a super keyword. and i've use that constructor to initialized those  instance variables

--- task8b/src/nonCitrus.java
+++ task8b/src/nonCitrus.java
@@ -1,7 +1,11 @@
-public class nonCirtus extends fruitObject{
+public class nonCitrus extends fruitObject {
     String FruitShape;
     String VitaminType;
     String Color;
-
-
+    public nonCitrus(String FruitShape, String VitaminType, String Color) {
+        super("Banana");
+        this.FruitShape = FruitShape;
+        this.VitaminType = VitaminType;
+        this.Color = Color;
+    }
 }
