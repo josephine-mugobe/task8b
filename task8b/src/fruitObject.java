ebd91c611e456fc6a95660570d06938d2e683e12
josephine-mugobe <josephinemunguacizamugobe@gmail.com>
2025-03-19 19:20:13 +0300

    commit#2
    In the NV on line 7 i've create an abstract method to display the details of each object. and on line 9 in the NV i create a concrete method to compute the cost value of each fruit

--- task8b/src/fruitObject.java
+++ task8b/src/fruitObject.java
@@ -1,8 +1,13 @@
 public class fruitObject {
     String name;
-    
+
     public fruitObject(String name) {
         this.name = name;
     }
+    public abstract void displayFruitDetails();
 
+    public double FruitCostPrice(double UnitPrice, int NumberOfPieces ){
+        double cost=UnitPrice*NumberOfPieces;
+        return cost;
+    }
 }
