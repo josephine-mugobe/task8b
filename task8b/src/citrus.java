6338714b3b0894bdc74cec48346050acede1e5bc
josephine-mugobe <josephinemunguacizamugobe@gmail.com>
2025-03-19 19:47:23 +0300

    commit#2
    in the NV on line 5 i've use the constructor that initialize the the common value and i've set the name of it using a super keyword. and i've use that constructor to initialized those  instance variables

--- task8b/src/citrus.java
+++ task8b/src/citrus.java
@@ -1,4 +1,10 @@
 public class citrus extends fruitObject{
     String FruitShape;
     String VitaminType;
+
+    public citrus(String FruitShape, String VitaminType) {
+        super("Orange");
+        this.FruitShape=FruitShape;
+        this.VitaminType=VitaminType;
+    }
 }
